b8e4c6d339f6967c40a2e86e899db4c86e3a4b21
Nikomi0824 <124908453+Nikomi0824@users.noreply.github.com>
2023-06-17 12:11:27 +0700

    Arxetectura

--- project_gazprom/src/router/index.js
+++ project_gazprom/src/router/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Page404 from '@/views/404/404Page.vue'
+import MainPage from '@/views/MainPage/MainPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path:'/',
+    name:'MainPage',
+    component: MainPage
+  },
+  {
+    path:'*',
+    name:'Page404',
+    component: Page404
+  },
 ]
 
 const router = new VueRouter({
---
+++ project_gazprom/src/views/MainPage/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+
+    </section>
+</template>
+
+
+<script>
+export default{
+    name:'MainPage'
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
